2163c52a22039548acbd87832cf7dc702cbf4650
Jingqing3948 <2351290287@qq.com>
2024-02-14 23:21:10 +0800

    m_mux21 switch

--- npc/csrc/auto_bind.cpp
+++ npc/csrc/auto_bind.cpp
@@ -2,7 +2,8 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
-	nvboard_bind_pin( &top->f, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
-	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
-	nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
+	nvboard_bind_pin( &top->y, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
+	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW2);
+	nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
+	nvboard_bind_pin( &top->s, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
 }
