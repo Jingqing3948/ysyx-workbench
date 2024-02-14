11370a8149f8d3440ff72d0b2d4adb0ef1286f2f
Jingqing3948 <2351290287@qq.com>
2024-02-14 23:33:52 +0800

    2-bit, 4-for-1 selector

--- npc/csrc/auto_bind.cpp
+++ npc/csrc/auto_bind.cpp
@@ -3,7 +3,6 @@
 
 void nvboard_bind_all_pins(Vtop* top) {
 	nvboard_bind_pin( &top->y, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
-	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW2);
-	nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
-	nvboard_bind_pin( &top->s, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
+	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 4, SW5, SW4, SW3, SW2);
+	nvboard_bind_pin( &top->s, BIND_RATE_SCR, BIND_DIR_IN , 2, SW1, SW0);
 }
